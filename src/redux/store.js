e862540324a9b39e01e23230eb2ba29d458c9e26
Yuliya1913 <Lera-855@email.ua>
2023-06-14 23:05:20 +0300

    add  middleware and not

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,25 +2,42 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import { persistedReducer } from './contacts/contactsSlice';
 import persistStore from 'redux-persist/es/persistStore';
-import {
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
 
 export const store = configureStore({
   reducer: {
     contacts: persistedReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
 
 export const persistor = persistStore(store);
+
+
+
+// с мидлвеа middleware прослойка для проверки данных
+
+// import { configureStore } from '@reduxjs/toolkit';
+
+// import { persistedReducer } from './contacts/contactsSlice';
+// import persistStore from 'redux-persist/es/persistStore';
+// import {
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+
+// export const store = configureStore({
+//   reducer: {
+//     contacts: persistedReducer,
+//   },
+//   middleware: getDefaultMiddleware =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+// });
+
+// export const persistor = persistStore(store);
